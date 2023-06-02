c88af2190f3ba99ea69cc69794ae5c7f4ee84570
vlfldr <steve@vlfldr.com>
2023-06-02 14:00:51 -0500

    Bootstrap

--- client/client.py
+++ client/client.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.get('/')
 def home():
-    return render_template('register.html', client={'clientName': ''})
+    return render_template('home.html')
 
 @app.get('/register')
-@app.post('/register')
-def register():
+def registerView():
     return render_template('register.html', client={'clientName': ''})
 
+@app.post('/register')
+def registerPost():
+    print(request.form['backupTime'])
+    return 'OK'
+
+@app.get('/login')
+def loginView():
+    return render_template('login.html')
+
+@app.post('/login')
+def loginPost():
+    print(request.form['clientName'])
+    return 'OK'
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', 9222, debug=True)
+    app.run('localhost', 9222, debug=True)
